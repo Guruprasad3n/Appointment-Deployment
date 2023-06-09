5c3f9dbc27f749517af09d3a506e1fed5a6163fe
Guru Prasad <104028016+Guruprasad3n@users.noreply.github.com>
2023-06-09 17:57:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const jwt = require("jsonwebtoken");
-const a = require("./frontend/build/index.html");
-// for Deployment
-const path = require("path");
+
+// listen Port
+const Port = process.env.PORT;
+
+/// for Deployment
+const path = require("path")
 // const path = require("./frontend/build/")
 
 // dotenv config
@@ -17,13 +20,14 @@ connectDB();
 // rest object
 const app = express();
 
+
 // middlewares
-app.use(cors());
+app.use(cors({origin:'*'}));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// routes
 
+// routes
 const userRoute = require("./routes/userRoutes");
 const adminRoute = require("./routes/adminRoute");
 const doctorRoute = require("./routes/DocRoute");
@@ -32,17 +36,101 @@ app.use("/api/v1/user", userRoute);
 app.use("/api/v1/admin", adminRoute);
 app.use("/api/v1/doctor", doctorRoute);
 
+
 // For Deployment Static FIle From Build or Public
-app.use(express.static(path.join(__dirname, "./frontend/build")));
-app.get("*", function (req, res) {
-  res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
-});
+// app.use(express.static(path.join(__dirname, "./frontend/build")))
+// app.get("*", function(req, res){
+// res.sendFile(path.join(__dirname, "./frontend/build/index.html"))
+// });
 // -----------------------------
 
-// listen Port
-const Port = process.env.PORT;
+
+
+
+
 
 // listen
-app.listen(Port, (req, res) => {
-  console.log(`Server Runnning in http://localhost:${Port}`);
-});
+// app.listen(Port, async( (req, res) => {
+//   console.log(`Server Runnning in http://localhost:${Port}`);
+// }));
+
+app.listen(Port, async()=>{
+  try{
+    await connectDB
+    console.log("connected")
+    console.log(`Server Runnning in http://localhost:${Port}`)
+  }
+  catch(e){
+console.log(e)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const express = require("express");
+// const cors = require("cors");
+// const dotenv = require("dotenv");
+// const jwt = require("jsonwebtoken");
+// const a = require("./frontend/build/index.html");
+// // for Deployment
+// const path = require("path");
+// // const path = require("./frontend/build/")
+
+// // dotenv config
+// dotenv.config();
+// // dbConnect()
+
+// // mongo Connectin``
+// const connectDB = require("./config/db");
+// connectDB();
+// // rest object
+// const app = express();
+
+// // middlewares
+// app.use(cors());
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+
+// // routes
+
+// const userRoute = require("./routes/userRoutes");
+// const adminRoute = require("./routes/adminRoute");
+// const doctorRoute = require("./routes/DocRoute");
+
+// app.use("/api/v1/user", userRoute);
+// app.use("/api/v1/admin", adminRoute);
+// app.use("/api/v1/doctor", doctorRoute);
+
+// // For Deployment Static FIle From Build or Public
+// app.use(express.static(path.join(__dirname, "./frontend/build")));
+// app.get("*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "./frontend/build/index.html"));
+// });
+// // -----------------------------
+
+// // listen Port
+// const Port = process.env.PORT;
+
+// // listen
+// app.listen(Port, (req, res) => {
+//   console.log(`Server Runnning in http://localhost:${Port}`);
+// });
+
+
+
+
+
+
+
